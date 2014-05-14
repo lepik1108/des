28eeb558061108ec21e7efcaaa0a461523e7d5b0
Alexey <lepik1108@gmail.com>
2014-05-14 12:43:25 +0300

    лоло

---
+++ lab8.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+class Plot(object):
+    def __init__(self):
+        self.poly = []
+        self.fig, self.ax = plt.subplots()
+        self.ax.axis([0, 10, 0, 10])
+        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+
+        plt.show()
+
+    def on_click(self, event):
+        if event.button == 1:
+            self.poly.append((event.xdata, event.ydata))
+        elif event.button == 3:
+            self.draw_poly()
+
+    def draw_poly(self):
+        self.ax.fill(*zip(*self.poly))
+        self.poly = []
+        self.fig.canvas.draw()
+
+Plot()
